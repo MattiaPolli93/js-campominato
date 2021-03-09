ede5c70d4c524604a373329e280e1ec5e07651f2
Mattia <matpolli93@gmail.com>
2021-03-09 17:59:56 +0100

    Main exercise completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Minefield Game
+// Minefield-like Game (with numbers)
 
 // Functions
 // Function to generate a random number
@@ -18,15 +18,56 @@ function isNumberInArray(number, numArray) {
 
 // Generating the 16 ("bomb") computer numbers (>= 1 && <= 100)
 // Variables
-var bombNumber = [];
+var bombNumbers = [];
 
-while (bombNumber.length < 16) {
+// Calculating
+while (bombNumbers.length < 16) {
     var number = randomNumber(1, 100);
 
-    if (!isNumberInArray(number, bombNumber)) {
-     bombNumber.push(number);
+    if (!isNumberInArray(number, bombNumbers)) {
+     bombNumbers.push(number);
     }
 }
 
 // Remove the following comment to cheat the game (by displaying the "bomb" numbers)
-// console.log ("These are the off-limits numbers! " + bombNumber);
+// console.log ("These are the off-limits numbers: " + bombNumbers);
+
+// User's victory/loss
+// Variables
+var userNumbers = [];
+var games = 84;
+var points = 0;
+var win = false;
+var lose = false;
+
+while (!win && !lose) {
+    // Asking user
+    do {
+        var userNumber = parseInt(prompt("Try to type as many numbers between 1 and 100 as you can"));
+    } while ((userNumber < 1 || userNumber > 100) || isNaN(userNumber));
+    
+    // Calculating
+    if (isNumberInArray(userNumber, userNumbers)) {
+        alert("Careful, you have already chosen this number!");
+    } else {
+        if (isNumberInArray(userNumber, bombNumbers)) {
+            lose = true;
+        } else {
+            userNumbers.push(userNumber);
+            points++;
+            if (userNumbers.length == games) {
+                win = true;
+            }
+        }
+    }
+}
+
+// Displaying user's inserted numbers
+console.log("These are your numbers: " + userNumbers);
+
+// Displayed messages (win/lose)
+if (win) {
+    alert("You won!");
+} else if (lose) {
+    alert("You lost...");
+}
