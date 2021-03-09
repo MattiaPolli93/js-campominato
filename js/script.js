85aef338d83e88274db139d14a7640eb3a2e3a2c
Mattia <matpolli93@gmail.com>
2021-03-09 18:08:19 +0100

    Stored functions on a different file

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
 // Minefield-like Game (with numbers)
 
-// Functions
-// Function to generate a random number
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);    
-}
-
-// Function to check if a number is present in an array
-function isNumberInArray(number, numArray) {
-    for (var i = 0; i < numArray.length; i++) {
-        if (number === numArray[i]) {
-            return true;
-        }
-    }
-    return false;
-}
-
 // Generating the 16 ("bomb") computer numbers (>= 1 && <= 100)
 // Variables
 var bombNumbers = [];
